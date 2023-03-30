844caf494ae603b75288e25b1749b1dc67a8c477
Christian Salvadora <christianrsalvadora@gmail.com>
2023-03-30 09:42:19 +0800

    Remove dfx files

--- .dfx/ic/canisters/first_canister/first_canister.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'get_count' : IDL.Func([], [IDL.Nat], []),
-    'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
